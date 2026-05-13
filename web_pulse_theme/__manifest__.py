8e117d399cc71bb0acf6086a4cd6cb9ab90b7354
Kuma Telila <kumatelila26@gmail.com>
2026-05-13 19:27:46 +0300

    Fix documentation

--- web_pulse_theme/__manifest__.py
+++ web_pulse_theme/__manifest__.py
@@ -15,6 +15,7 @@ Key features include a persistent vertical apps sidebar, a redesigned two-row na
     "license": "OPL-1",
     "price": 45.00,
     "currency": "USD",
+    'live_test_url' : 'http://37.60.243.252:8076/',
      'images': [
         'static/description/banner.png',
         'static/description/theme_screenshot.png',
